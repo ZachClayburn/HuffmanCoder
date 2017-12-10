1de4d8435d92d624331dda45db804e6779f3c39e
Zach Clayburn <zachclayburn@gmail.com>
2017-12-09 17:49:08 -0700

    File now reads in and passes a dictionary to the tree constructor

--- HuffmanCoder.py
+++ HuffmanCoder.py
@@ -1,16 +1,32 @@
-
 import HuffmanTree as Tree
+
 
 class HuffmanCoder:
     def __init__(self):
         self.tree = None
 
-    def build_tree(self,file_name):
-        file = open(file_name)
-        text = file.read()
+    def build_tree(self, file_name):
+        try:
+            file = open(file_name, 'r')
+            text = file.read()
+        except FileNotFoundError:
+            print('File Not Found\n No tree created.')
+            return False
+        file.close()
 
-        
+        char_count = dict()
+        for char in text:
+            if char in char_count.keys():
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
+
+        self.tree = Tree.HuffmanTree(char_count)
+
 
 
 if __name__ == "__main__":
-    coder = HuffmanCoder
+    coder = HuffmanCoder()
+    coder.build_tree('TestFiles/test1.txt')
+
+
--- HuffmanTree.py
+++ HuffmanTree.py
@@ -4,3 +4,5 @@ import HuffmanNode as Node
 class HuffmanTree:
     def __init__(self,char_counts):
         self.root = None
+        for thing in char_counts:
+            print(thing)
